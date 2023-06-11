5e3ccb49befd4bb356dc348414eb88674cee12f7
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-06-11 16:08:40 +0300

    Test

--- store/models.py
+++ store/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-
 # Create your models here.
-
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
