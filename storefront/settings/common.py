6922be720964501853a75a7205ea44671495a2a3
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-07-07 08:51:58 +0300

    deployment wip

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -162,12 +162,8 @@ SIMPLE_JWT = {
    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120)
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 2525
-DEFAULT_FROM_EMAIL = 'from@madis.com'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 
 ADMINS = [
     ('Bob', 'admin@bob.com')
--- storefront/settings/pro.py
+++ storefront/settings/pro.py
@@ -1,10 +1,19 @@
 from .common import *
 import os
+import dj_database_url
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bobbuy-prod-bff4f84a128d.herokuapp.com']
 
+DATABASES = {
+    'default': dj_database_url.config() # This module looks for an env variable called DATABASE_URL
+}
 
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'from@madis.com'
